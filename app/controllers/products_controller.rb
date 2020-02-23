c86233d9e3ac6b2456ad0bec00c3004991bdb15c
takanari-y <tkyamamura55@gmail.com>
2020-02-23 09:33:57 +0000

    #2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
   def index
+  	@product=Product.all
+  	@categories=Categories.all
   end
 
   def show
+  	@categories=Categories.all
+  	@product=Product.find(params[:id])
   end
+
+   private
+  def product_params
+    params.require(:product).permit(:name, :content, :price, :product_image, :is_active)
+end
 end
