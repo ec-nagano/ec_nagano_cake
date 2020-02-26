a68bd6175921f6518dae8ed8ad21b502741f4e79
tama <yuki023201025@gmail.com>
2020-02-27 02:03:15 +0900

    deviseの新規登録時の使えるカラムの追加、ログイン後の画面の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
     $delivery_fee = 800
     $tax_rate = 0.1
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+	protected
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :postcode, :phone_number])
+	  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :postcode, :phone_number])
+	end
+
+	def after_sign_in_path_for(resource)
+	  root_path
+	end
+	def after_sign_out_path_for(resource)
+	  root_path
+	end
 end
