29cac31c50336e521cacc545bf5a1949c7e06df9
tama <yuki023201025@gmail.com>
2020-02-26 22:04:05 +0900

    admin/order index show controllerの作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,61 +7,52 @@ class Admin::OrdersController < ApplicationController
 		@order = Order.find(id: params[:id])
 		#customerの名前が必要だから
 		@customer = @order.customer
-
-		#assosiationでやってみる
-		#下の２つは同じ意味
 		@ordered_items = Ordered_item.where(order_id: @order.id)
-		@ordered_items_asso = @order.ordered_items
 	end
 
 	def update
-		#注文ステの入金確認がきたら自動で制作ステを制作待ちにする
-		if params[:order][:status] && params[:order][:status] == "入金確認"
-			@order = Order.find(params[:id])
-			@order.update(order_params)
+		@order = Order.find(params[:id])
+		@ordered_item = Ordered_item.find_by(id: params[:item][:item_id])
+		@ordered_item.status = params[:item][:status]
+
+		@order.update(order_params)
+		@ordered_item.update
+
+		#注文ステの入金確認1がきたら自動で制作ステを制作待ち1にする
+		if @order.status == 1
 
 			@order_items = @order.ordered_items
-			@order_items.to_a.map! do |item|
-				item.status == "制作待ち"
+			@order_items.each do |item|
+				item.status = 1
+				item.update
 			end
-			@order_items.update(ordered_item_params)
 			redirect_to("/admin/orders/#{@order.id}")
 		end
 
+		#一つの制作ステの製作中2がきたら自動で注文ステを製作中2にする
+		#どっちか、うまくいった方
+		# order_items = @order.ordered_items
+		# if order_items.any? { |item| item.status == 2 }
+		# 	@order.status = 2
+		# 	@order.update
+		# 	redirect_to("/admin/orders/#{@order.id}")
+		# end
+		if @ordered_item.status == 2
 
-		#一つの制作ステの製作中がきたら自動で注文ステを製作中にする
-		if params[:ordered_item][:status] && params[:ordered_item][:status] == "制作中"
-			@order = Order.find(params[:id])
-			#多分違う
-			@ordered_item = Ordered_item.find_by(order_id: @order.id)
-			@ordered_item.updatee(ordered_item_params)
-
-			@order.status == "製作中"
-			@order.update(order_params)
+			@order.status = 2
+			@order.update
 			redirect_to("/admin/orders/#{@order.id}")
 		end
 
-		#全ての制作ステの製作完了がきたら自動で注文ステを制作完了にする
-		if params[:ordered_item][:status] && params[:ordered_item][:status] == "制作完了"
-			@order = Order.find(params[:id])
-			#多分違う
-			@ordered_item = Ordered_item.find_by(order_id: @order.id)
-			@ordered_item.updatee(ordered_item_params)
+		#全てのステの製作完了3がきたら自動で注文ステを発送準備中3にする
+		ordered_items = @order.ordered_items
+		if ordered_items.all? { |item| item.status == 3 }
 
-			@order.status == "制作完了"
-			@order.update(order_params)
+			@order.status = 3
+			@order.update
 			redirect_to("/admin/orders/#{@order.id}")
 		end
 
-		#一つの注文を更新するならこれだけでok
-		@order = Order.find(params[:id])
-		@order.update(order_params)
-		redirect_to("/admin/orders/#{@order.id}")
-
-		#一つの商品を選んで更新できない
-		@order = Order.find(params[:id])
-		@ordered_item = Ordered_item.find_by(order_id: @order.id)
-		@order.update(ordered_item_params)
 		redirect_to("/admin/orders/#{@order.id}")
 	end
 
