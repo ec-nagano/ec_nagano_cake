c904335ddacc1a850a7fab312a8fe2a53c8a2685
tama <yuki023201025@gmail.com>
2020-02-26 23:05:19 +0900

    admin/homesのtopとcontrollerを作成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomesController < ApplicationController
+	before_action :authenticate_admin!
 	def top
+		#本日の注文の呼び出し where
+		@orders = Order.where(created_at: Time.current.beginning_od_day..Time.current.end_of_day)
 	end
 end
