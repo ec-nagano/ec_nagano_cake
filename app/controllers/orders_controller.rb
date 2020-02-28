1c5b5ed4e7aa10ef1836e8121ad82fde305ddf7c
stat-ki <per_ik@outlook.com>
2020-02-28 09:02:11 +0000

    #114 order, cart_item のコントローラを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,61 +10,77 @@ class OrdersController < ApplicationController
 
     def show
         @order = Order.find(params[:id])
-        @ordered_items = OrderedItems.where(order_id: @order.id)
+        @ordered_items = OrderedItem.where(order_id: @order.id)
     end
 
     def new
-        @addresses = ShppingAddress.where(customer_id: current_customer.id)
+        cart_items = CartItem.where(customer_id: current_customer.id)
+        if cart_items.blank?
+            flash[:notice] = "カートに商品がありません"
+            redirect_to(products_path)
+        end
+        @addresses = ShippingAddress.where(customer_id: current_customer.id)
     end
 
     def confirm
+        if params[:payment].blank? || params[:address_type].blank?
+            flash[:notice] = "未入力の項目があります。"
+            cart_items = CartItem.where(customer_id: current_customer.id)
+            @addresses = ShippingAddress.where(customer_id: current_customer.id)
+            render(:new)
+        end
+        @order = Order.new
         @cart_items = CartItem.where(customer_id: current_customer.id)
-        # 選択されたラジオボタンによってパラメーターを変化する
-        case "address_type"
+        # 選択されたラジオボタンによってインスタンス変数に代入する値を変化する
+        case params[:address_type].to_i
         # "ご自身の住所"の場合
         when 0
-            params[:order][:postcode] = current_customer.postcode
-            params[:order][:address] = current_customer.address
-            params[:order][:recipient] = "#{current_customer.first_name} + ' ' + #{current_customer.last_name}"
+            @postcode = current_customer.postcode
+            @address = current_customer.address
+            @recipient = current_customer.first_name + ' ' + current_customer.last_nam
         # "登録済み住所"の場合
         when 1
-            address = ShppingAddress.find(params[:existing_address])
-            params[:order][:postcode] = address.postcode
-            params[:order][:address] = address.address
-            params[:order][:recipient] = address.name
+            address = ShippingAddress.find(params[:existing_address])
+            @postcode = address.postcode
+            @address = address.address
+            @recipient = address.name
         # "新しい住所"の場合    
         when 2
-            params[:order][:postcode] = params[:new_postcode]
-            params[:order][:address] = params[:new_address]
-            params[:order][:recipient] = params[:new_recipient]
+            address = ShippingAddress.new(
+                customer_id: current_customer.id,
+                postcode: params[:new_postcode],
+                address: params[:new_address],
+                name: params[:new_recipient]
+            )
+            address.save
+            flash[:notice] = "住所を登録しました。"
+            @postcode = address.postcode
+            @address = address.address
+            @recipient = address.name
         end
         params[:order][:delivery_fee] = $delivery_fee
         # 請求額を計算する処理
+        @billing_amount = 0
         @cart_items.each do |e|
-            billing_amount += (e.product.price * (1 + $tax_rate)).round
+            @billing_amount += (e.product.price * (1 + $tax_rate)).round * e.amount
         end
-        params[:order][:billing_amount] = billing_amount
-        params[:order][:payment] = params[:payment]
-        @order = Order.new(order_params)
-        @order.customer_id = current_customer.id
-        unless @order.valid?
-            @addresses = ShppingAddress.where(customer_id: current_user.id)
-            render(:new)
-        end
+        @delivery_fee = $delivery_fee
+        @payment = params[:payment].to_i
     end
 
     def create
         # ordersテーブルのレコードを作成する処理
+        params[:order][:payment] = params[:order][:payment].to_i
         order = Order.new(order_params)
         order.customer_id = current_customer.id
         order.save
         # ordered_itemsテーブルのレコードを作成する処理
-        @cart_items = CartItem.where(customer_id: current_customer.id)
-        @cart_items.each do |e|
-            ordered_item = OrderedItems.new(
-                order_id: @order.id,
-                product_id: e.id,
-                price: (e.price * (1 * $tax_rate)).round,
+        cart_items = CartItem.where(customer_id: current_customer.id)
+        cart_items.each do |e|
+            ordered_item = OrderedItem.new(
+                order_id: order.id,
+                product_id: e.product.id,
+                price: (e.product.price * (1 * $tax_rate)).round,
                 amount: e.amount
             )
             ordered_item.save
