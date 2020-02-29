8440bc6e5a25c3fbc787ce88a6785ae73615f6b9
tama <yuki023201025@gmail.com>
2020-02-29 17:36:28 +0900

    コメントのつけたし

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,12 +14,9 @@ class Admin::OrdersController < ApplicationController
 	def update
 		order = Order.find(params[:id])
 
-		# ordered_item = OrderedItem.find_by(id: params[:item][:item_id])
-		# ordered_item.status = params[:item][:status]
-		# ordered_items = @order.ordered_items#all?とany?で使う
-
 		#注文ステータスの分岐
 		if params[:order]
+			#f.selectから送られてくるparamsはstringなのでintegerに変換する
 			params[:order][:status] = params[:order][:status].to_i
 
 			case params[:order][:status]
@@ -29,7 +26,6 @@ class Admin::OrdersController < ApplicationController
 				order.update(order_params)
 				ordered_items = order.ordered_items
 				ordered_items.each do |item|
-					#item.status = 1
 					item.update(status: 1)
 				end
 				redirect_to("/admin/orders/#{order.id}")
@@ -38,6 +34,7 @@ class Admin::OrdersController < ApplicationController
 				order.update(order_params)
 				redirect_to("/admin/orders/#{order.id}")
 			end
+
 		#製作ステータスの分岐
 		else params[:ordered_item]
 			params[:ordered_item][:status] = params[:ordered_item][:status].to_i
@@ -46,7 +43,6 @@ class Admin::OrdersController < ApplicationController
 			#一つの製作ステの製作中2がきたら自動で注文ステを製作中2にする
 			when 2
 				ordered_item = OrderedItem.find_by(id: params[:ordered_item][:item_id])
-				#ordered_item.status = params[:ordered_item][:status]
 				ordered_item.update(ordered_item_params)
 
 				order.update(status: 2)
@@ -54,10 +50,10 @@ class Admin::OrdersController < ApplicationController
 			#全ての製作ステの製作完了3がきたら自動で注文ステを発送準備中3にする
 			when 3
 				ordered_item = OrderedItem.find_by(id: params[:ordered_item][:item_id])
-				#ordered_item.status = params[:ordered_item][:status]
 				ordered_item.update(ordered_item_params)
 
 				ordered_items = order.ordered_items
+				#item.statusだと定義したstring型の方が呼ばれるので_before_type_castをつけてintegerにする
 				if ordered_items.all? { |item| item.status_before_type_cast == 3 }
 					order.update(status: 3)
 				end
