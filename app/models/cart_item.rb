915da7758bde0e2f44c3bdbe04dfc0cde3b6aa73
tama <yuki023201025@gmail.com>
2020-03-01 17:07:59 +0900

    modelのvalidates

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,5 @@ class CartItem < ApplicationRecord
 	belongs_to :customer
 	belongs_to :product
 
-	validates :amount, presence: true
+	validates :amount, presence: true, numericality: { greater: 1 }
 end
