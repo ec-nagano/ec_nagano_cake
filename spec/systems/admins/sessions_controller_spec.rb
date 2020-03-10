6ef86c1897e9d458d4733354fd4365e92c82c3f9
stat-ki <per_ik@outlook.com>
2020-03-10 12:44:05 +0000

    admin#sessions のテストファイルを修正

--- spec/systems/admins/sessions_controller_spec.rb
+++ spec/systems/admins/sessions_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Admins::SessionsController, type: :controller do
-    
-    describe "GET #new" do
-        it "returns http success" do
-          get :new
-          expect(response).to have_http_status(:success)
-        end
+RSpec.feature "admin/sessions_controller", type: :system do
+  describe 'admin/sessions#new' do
+    before do
+      visit new_admin_session_path
+      fill_in 'admin[email]', with: 'ec_nagano_cake@email'
+      fill_in 'admin[password]', with: 'password'
+      click_button 'ログイン'
+    end
+    context 'Confirm Sign In' do
+      it 'Sign In' do
+        visit admin_path
+        expect(page).to have_content '管理者画面'
       end
+    end
+  end
 
 end
