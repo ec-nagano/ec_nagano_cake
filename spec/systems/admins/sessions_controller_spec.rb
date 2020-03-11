f0ab2284f07c983df0fbb417af659800dccf4a5f
stat-ki <per_ik@outlook.com>
2020-03-11 08:15:21 +0000

    #231 admin/sessions テストファイルを修正, gemを導入

--- spec/systems/admins/sessions_controller_spec.rb
+++ spec/systems/admins/sessions_controller_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "admin/sessions_controller", type: :system do
-  describe 'admin/sessions#new' do
-    before do
-      visit new_admin_session_path
-      fill_in 'admin[email]', with: 'ec_nagano_cake@email'
-      fill_in 'admin[password]', with: 'password'
-      click_button 'ログイン'
-    end
-    context 'Confirm Sign In' do
-      it 'Sign In' do
+  before do
+    @admin = create(:admin)
+    visit new_admin_session_path
+    fill_in 'admin_email', with: @admin.email
+    fill_in 'admin_password', with: @admin.password
+    click_button 'ログイン'
+  end
+  describe 'admin/sessions' do
+    context 'Confirm functions' do
+      it 'Sign in' do
         visit admin_path
         expect(page).to have_content '管理者画面'
+      end
+      it 'Sign out' do
+        visit admin_path
+        click_link 'ログアウト'
+        expect(page).to have_content 'ログイン'
       end
     end
   end
